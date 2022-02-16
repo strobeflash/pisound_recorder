b9a42f8a3d46b3c76bae7ac8409ceec3a994bd99
Olivier Magloire <omagloire@posteo.net>
2022-02-16 10:32:51 +0100

    add alternative blinking during conversion

--- recording.sh
+++ recording.sh
@@ -6,6 +6,7 @@ if [[ ! $(/usr/bin/systemctl is-active recording.service) = "active" ]]; then
 	periodic_led_blink 3 1
 	/usr/bin/systemctl start recording
 else
+	periodic_led_blink 3 0.5
 	/usr/bin/systemctl stop recording
 	periodic_led_blink 0 0 
 fi
